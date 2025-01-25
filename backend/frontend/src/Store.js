c9f27207b0d7449bae5d2c4607e42d8d32da2579
tulsi <kumari20tulsi@gmail.com>
2025-01-26 01:08:47 +0530

    store changed

--- backend/frontend/src/Store.js
+++ backend/frontend/src/Store.js
@@ -1,44 +1,117 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import {thunk} from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { newProductReducer, newReviewReducer, productDetailsReducer, productReducer, productReviewsReducer, productsReducer, reviewReducer } from "./reducers/productReducer";
+// import { createStore, combineReducers, applyMiddleware } from "redux";
+// import {thunk} from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import { newProductReducer, newReviewReducer, productDetailsReducer, productReducer, productReviewsReducer, productsReducer, reviewReducer } from "./reducers/productReducer";
 
+
+// import {
+//   allUsersReducer,
+//   forgotPasswordReducer,
+//     // allUsersReducer,
+//     // forgotPasswordReducer,
+//     profileReducer,
+//     userDetailsReducer,
+//     // userDetailsReducer,
+//     userReducer,
+//   } from "./reducers/userReducer";
+// import { cartReducer } from "./reducers/cartReducer";
+// import { allOrdersReducer, myOrdersReducer, newOrderReducer, orderDetailsReducer, orderReducer } from "./reducers/orderReducer";
+
+
+// const reducer=combineReducers({
+//  products: productsReducer,
+//  productDetails: productDetailsReducer,
+//  user: userReducer,
+//  profile:profileReducer,
+//  forgotPassword:forgotPasswordReducer,
+//  cart:cartReducer,
+//  newOrder: newOrderReducer,
+//  myOrders: myOrdersReducer,
+//  orderDetails: orderDetailsReducer,
+//  newReview: newReviewReducer,
+//  allOrders: allOrdersReducer,
+//  order: orderReducer,
+//  product: productReducer,
+//  newProduct: newProductReducer,
+// allUsers: allUsersReducer,
+// userDetails: userDetailsReducer,
+// productReviews: productReviewsReducer,
+//   review: reviewReducer,
+// });
+
+// let initialState = {
+//   cart: {
+//     cartItems: localStorage.getItem("cartItems")
+//       ? JSON.parse(localStorage.getItem("cartItems"))
+//       : [],
+//     shippingInfo: localStorage.getItem("shippingInfo")
+//       ? JSON.parse(localStorage.getItem("shippingInfo"))
+//       : {},
+//   },
+// };
+// const middleware=[thunk];
+
+// const store=createStore(reducer,
+//     initialState,
+//     composeWithDevTools(applyMiddleware(...middleware))
+
+// );
+
+
+// export default store;
+
+import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import {thunk} from "redux-thunk";
+import {
+  newProductReducer,
+  newReviewReducer,
+  productDetailsReducer,
+  productReducer,
+  productReviewsReducer,
+  productsReducer,
+  reviewReducer,
+} from "./reducers/productReducer";
 
 import {
   allUsersReducer,
   forgotPasswordReducer,
-    // allUsersReducer,
-    // forgotPasswordReducer,
-    profileReducer,
-    userDetailsReducer,
-    // userDetailsReducer,
-    userReducer,
-  } from "./reducers/userReducer";
+  profileReducer,
+  userDetailsReducer,
+  userReducer,
+} from "./reducers/userReducer";
 import { cartReducer } from "./reducers/cartReducer";
-import { allOrdersReducer, myOrdersReducer, newOrderReducer, orderDetailsReducer, orderReducer } from "./reducers/orderReducer";
+import {
+  allOrdersReducer,
+  myOrdersReducer,
+  newOrderReducer,
+  orderDetailsReducer,
+  orderReducer,
+} from "./reducers/orderReducer";
 
-
-const reducer=combineReducers({
- products: productsReducer,
- productDetails: productDetailsReducer,
- user: userReducer,
- profile:profileReducer,
- forgotPassword:forgotPasswordReducer,
- cart:cartReducer,
- newOrder: newOrderReducer,
- myOrders: myOrdersReducer,
- orderDetails: orderDetailsReducer,
- newReview: newReviewReducer,
- allOrders: allOrdersReducer,
- order: orderReducer,
- product: productReducer,
- newProduct: newProductReducer,
-allUsers: allUsersReducer,
-userDetails: userDetailsReducer,
-productReviews: productReviewsReducer,
+// Combine all reducers
+const reducer = combineReducers({
+  products: productsReducer,
+  productDetails: productDetailsReducer,
+  user: userReducer,
+  profile: profileReducer,
+  forgotPassword: forgotPasswordReducer,
+  cart: cartReducer,
+  newOrder: newOrderReducer,
+  myOrders: myOrdersReducer,
+  orderDetails: orderDetailsReducer,
+  newReview: newReviewReducer,
+  allOrders: allOrdersReducer,
+  order: orderReducer,
+  product: productReducer,
+  newProduct: newProductReducer,
+  allUsers: allUsersReducer,
+  userDetails: userDetailsReducer,
+  productReviews: productReviewsReducer,
   review: reviewReducer,
 });
 
+// Initial state
 let initialState = {
   cart: {
     cartItems: localStorage.getItem("cartItems")
@@ -49,13 +122,14 @@ let initialState = {
       : {},
   },
 };
-const middleware=[thunk];
 
-const store=createStore(reducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
-
-);
-
+// Create the store using `configureStore`
+const store = configureStore({
+  reducer, // Pass the combined reducer
+  preloadedState: initialState, // Set initial state
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(thunk), // Add thunk middleware
+  devTools: process.env.NODE_ENV !== "production", // Enable Redux DevTools only in development
+});
 
 export default store;
